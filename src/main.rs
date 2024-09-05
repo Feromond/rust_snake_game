e86cbd6c14625cf068c96edaf6c9f011ca04e069
Feromond <Jacobpmish@gmail.com>
2024-09-05 09:43:04 -0600

    Added resizable windows and modified the game to allow for dynamic resolutions (still not working smoothly yet)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ggez::event::{self, EventHandler};
-use ggez::graphics::{self, Color, DrawMode, DrawParam, Mesh, MeshBuilder, Rect, Text};
+use ggez::graphics::{self, Canvas, Color, DrawMode, DrawParam, Mesh, MeshBuilder, Rect, Text};
 use ggez::input::keyboard::{KeyCode, KeyInput};
 use ggez::{mint, Context, GameResult};
 use nalgebra as na;
@@ -7,8 +7,6 @@ use rand::Rng;
 
 const SNAKE_SIZE: f32 = 50.0;
 const MOVE_TIME: f32 = 0.075; // Time in seconds between moves
-const WINDOW_WIDTH: f32 = 1600.0; // Set the window width
-const WINDOW_HEIGHT: f32 = 1000.0; // Set the window height
 
 enum GameMode {
     Menu,
@@ -31,31 +29,36 @@ struct GameState {
     score: i32,
     high_score: i32,
     mode: GameMode,
+    window_width: f32,
+    window_height: f32,
 }
 
 impl GameState {
-    fn new(_ctx: &mut Context) -> GameResult<GameState> {
+    fn new(ctx: &mut Context) -> GameResult<GameState> {
+        let (window_width, window_height) = GameState::get_window_size(ctx);
         let s = GameState {
             snake_body: vec![SnakeSegment {
                 pos: na::Point2::new(200.0, 200.0),
             }],
             food: Food {
-                pos: GameState::get_random_food_position(),
+                pos: GameState::get_random_food_position(window_width, window_height),
             },
             velocity: na::Vector2::new(SNAKE_SIZE, 0.0),
             last_update: 0.0,
             score: 0,
             high_score: 0,
             mode: GameMode::Menu,
+            window_width,
+            window_height,
         };
         Ok(s)
     }
 
-    fn get_random_food_position() -> na::Point2<f32> {
+    fn get_random_food_position(window_width: f32, window_height: f32) -> na::Point2<f32> {
         let mut rng = rand::thread_rng();
         na::Point2::new(
-            (rng.gen_range(0..(WINDOW_WIDTH as u32 / SNAKE_SIZE as u32)) as f32) * SNAKE_SIZE,
-            (rng.gen_range(0..(WINDOW_HEIGHT as u32 / SNAKE_SIZE as u32)) as f32) * SNAKE_SIZE,
+            (rng.gen_range(0..(window_width as u32 / SNAKE_SIZE as u32)) as f32) * SNAKE_SIZE,
+            (rng.gen_range(0..(window_height as u32 / SNAKE_SIZE as u32)) as f32) * SNAKE_SIZE,
         )
     }
 
@@ -63,14 +66,27 @@ impl GameState {
         self.snake_body = vec![SnakeSegment {
             pos: na::Point2::new(200.0, 200.0),
         }];
-        self.food.pos = GameState::get_random_food_position();
+        self.food.pos = GameState::get_random_food_position(self.window_width, self.window_height);
         self.velocity = na::Vector2::new(SNAKE_SIZE, 0.0);
         self.score = 0;
+    }
+
+    fn get_window_size(ctx: &mut Context) -> (f32, f32) {
+        let canvas = Canvas::from_frame(ctx, Some(Color::BLACK));
+        let rect = canvas.screen_coordinates().unwrap();
+        (rect.w, rect.h)
+    }
+
+    fn handle_resize(&mut self, ctx: &mut Context) {
+        let (window_width, window_height) = GameState::get_window_size(ctx);
+        self.window_width = window_width;
+        self.window_height = window_height;
     }
 }
 
 impl EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.handle_resize(ctx);
         match self.mode {
             GameMode::Menu => {
                 if self.score > self.high_score {
@@ -98,7 +114,10 @@ impl EventHandler for GameState {
 
                         // Ensure the new food does not spawn on the snake
                         loop {
-                            self.food.pos = GameState::get_random_food_position();
+                            self.food.pos = GameState::get_random_food_position(
+                                self.window_width,
+                                self.window_height,
+                            );
                             if !self
                                 .snake_body
                                 .iter()
@@ -119,8 +138,8 @@ impl EventHandler for GameState {
                     let head_pos = self.snake_body[0].pos;
                     if head_pos.x < 0.0
                         || head_pos.y < 0.0
-                        || head_pos.x >= WINDOW_WIDTH
-                        || head_pos.y >= WINDOW_HEIGHT
+                        || head_pos.x >= self.window_width
+                        || head_pos.y >= self.window_height
                     {
                         self.mode = GameMode::Menu;
                     }
@@ -140,8 +159,8 @@ impl EventHandler for GameState {
                 canvas.draw(
                     &menu_text,
                     DrawParam::default().dest(mint::Point2 {
-                        x: WINDOW_WIDTH * 0.5 - 350.0,
-                        y: WINDOW_HEIGHT * 0.4,
+                        x: self.window_width * 0.5 - 350.0,
+                        y: self.window_height * 0.4,
                     }),
                 );
                 let mut menu_high_score_text =
@@ -151,8 +170,8 @@ impl EventHandler for GameState {
                     &menu_high_score_text,
                     DrawParam::default()
                         .dest(mint::Point2 {
-                            x: WINDOW_WIDTH * 0.5 - 350.0,
-                            y: WINDOW_HEIGHT * 0.55,
+                            x: self.window_width * 0.5 - 350.0,
+                            y: self.window_height * 0.55,
                         })
                         .color(Color::from_rgb(0, 255, 0)),
                 )
@@ -211,27 +230,25 @@ impl EventHandler for GameState {
                     ctx.request_quit();
                 }
             }
-            GameMode::Playing => {
-                match key.keycode {
-                    Some(KeyCode::Right) | Some(KeyCode::D) if self.velocity.x == 0.0 => {
-                        self.velocity = na::Vector2::new(SNAKE_SIZE, 0.0);
-                    }
-                    Some(KeyCode::Left) | Some(KeyCode::A) if self.velocity.x == 0.0 => {
-                        self.velocity = na::Vector2::new(-SNAKE_SIZE, 0.0);
-                    }
-                    Some(KeyCode::Up) | Some(KeyCode::W) if self.velocity.y == 0.0 => {
-                        self.velocity = na::Vector2::new(0.0, -SNAKE_SIZE);
-                    }
-                    Some(KeyCode::Down) | Some(KeyCode::S) if self.velocity.y == 0.0 => {
-                        self.velocity = na::Vector2::new(0.0, SNAKE_SIZE);
-                    }
-                    Some(KeyCode::Escape) => {
-                        // Return to menu
-                        self.mode = GameMode::Menu;
-                    }
-                    _ => {}
+            GameMode::Playing => match key.keycode {
+                Some(KeyCode::Right) | Some(KeyCode::D) if self.velocity.x == 0.0 => {
+                    self.velocity = na::Vector2::new(SNAKE_SIZE, 0.0);
                 }
-            }
+                Some(KeyCode::Left) | Some(KeyCode::A) if self.velocity.x == 0.0 => {
+                    self.velocity = na::Vector2::new(-SNAKE_SIZE, 0.0);
+                }
+                Some(KeyCode::Up) | Some(KeyCode::W) if self.velocity.y == 0.0 => {
+                    self.velocity = na::Vector2::new(0.0, -SNAKE_SIZE);
+                }
+                Some(KeyCode::Down) | Some(KeyCode::S) if self.velocity.y == 0.0 => {
+                    self.velocity = na::Vector2::new(0.0, SNAKE_SIZE);
+                }
+                Some(KeyCode::Escape) => {
+                        // Return to menu
+                    self.mode = GameMode::Menu;
+                }
+                _ => {}
+            },
         }
         Ok(())
     }
@@ -240,7 +257,12 @@ impl EventHandler for GameState {
 fn main() -> GameResult {
     let (mut ctx, event_loop) = ggez::ContextBuilder::new("Rust Snake Game", "Jacob Mish")
         .window_setup(ggez::conf::WindowSetup::default().title("Snake Game"))
-        .window_mode(ggez::conf::WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT))
+        .window_mode(
+            ggez::conf::WindowMode::default()
+                .dimensions(1300.0, 750.0)
+                .min_dimensions(1300.0, 750.0)
+                .resizable(true),
+        )
         .build()?;
 
     let state = GameState::new(&mut ctx)?;
